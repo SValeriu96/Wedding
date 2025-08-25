84ffcc73ab0fc9f525c8b899afe7ff8ba6297b0f
Valeriu <158054858+SValeriu96@users.noreply.github.com>
2025-08-25 09:04:48 +0300

    Add files via upload

---
+++ services/assets/script/main.js
@@ -0,0 +1,8 @@
+const questions = document.querySelectorAll(".faq-question");
+
+questions.forEach((q) => {
+  q.addEventListener("click", () => {
+    const answer = q.nextElementSibling;
+    answer.style.display = answer.style.display === "block" ? "none" : "block";
+  });
+});
